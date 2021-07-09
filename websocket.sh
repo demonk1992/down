204b35e7f1e1563abd0b179d6bbaa681d2e44bc9
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-07-09 23:41:48 +0700

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cleae
-echo Installing Websocket-SSH AUTOSCRIPT BY HABLESSH
+echo Installing Websocket-SSH-OPENVPN
 sleep 1
 echo Cek Hak Akses...
 sleep 0.5
 cd
 #Install system auto run
 wget -O /etc/systemd/system/ws-ssh.service https://raw.githubusercontent.com/demonk1992/down/main/ws-ssh.service && chmod +x /etc/systemd/system/ws-ssh.service
-wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/Aantonberkentod/AutoScriptSSH/834e18746c6a3d822aef0a65cc9f3ec470bb6e52/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
+wget -O /etc/systemd/system/ws-ovpn.service https://raw.githubusercontent.com/demonk1992/down/ws-ovpn.service && chmod +x /etc/systemd/system/ws-ovpn.service
 #Install Websocket-SSH
 
 wget -O /usr/local/bin/ws-ssh https://raw.githubusercontent.com/demonk1992/down/main/ws-ssh && chmod +x /usr/local/bin/ws-ssh
